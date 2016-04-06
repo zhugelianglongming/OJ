62a081fecab71f594e78bf6e56298702e6296de0
zhugelianglongming <zhugelianglongming@163.com>
2016-04-06 13:28:59 +0800

    vector based hash map

---
+++ LeetCode/3_2.cpp
@@ -0,0 +1,23 @@
+/*
+* 16 ms
+* O(n)
+*/
+class Solution {
+public:
+	int lengthOfLongestSubstring(string s) {
+		vector<int> preIndex(256, -1);
+		int start = -1;
+		int maxL = 0;
+		for (int i = 0; i < s.length(); i++)
+		{
+			char ch = s[i];
+			if (preIndex[ch] > start)
+			{
+				start = preIndex[ch];
+			}
+			preIndex[ch] = i;
+			maxL = max(maxL, i - start);
+		}
+		return maxL;
+	}
+};
