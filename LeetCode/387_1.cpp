457cc32cb98ce0077249c85a5e85d0e1c6a38aad
zhugelianglongming <zhugelianglongming@users.noreply.github.com>
2016-08-27 16:45:29 +0800

    387. First Unique Character in a String

---
+++ LeetCode/387_1.cpp
@@ -0,0 +1,12 @@
+/*
+	* 64ms
+	* T: O(n)
+	* S: O(1)
+	*/
+	int firstUniqChar(string s) {
+		int c[128] = {0};
+		for(char ch : s) ++c[ch];
+		for(int i=0; i<s.length(); ++i)
+			if(1 == c[s[i]]) return i;
+		return -1;
+	}
